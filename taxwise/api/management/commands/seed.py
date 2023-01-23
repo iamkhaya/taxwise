7eec2ee318a1b8ba2997d7e68a08390d84bbcc0d
Khayelihle Tshuma <khayelihle.tshuma@gmail.com>
2023-01-23 09:21:51 +0200

    attempt to improve - working

--- taxwise/api/management/commands/seed.py
+++ taxwise/api/management/commands/seed.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import tabula
 from django.core.management.base import BaseCommand
-
+from tabulate import tabulate
 from taxwise.api.models import ChapterSection, Tarriff
 
 
@@ -12,162 +12,222 @@ class Command(BaseCommand):
 
     help = "Generate TOTP code for use with TOTP login"
 
+    PDF_FILE_NAME = "zimra-2022.pdf"
+    PDF_PAGE_RANGE = "990-1050"
+
+    def map_columns(self, num_columns):
+        # if num_columns == 4:
+        #     return ["Commodity Code", "Description", "Quantity", "General"]
+        if num_columns == 5:
+            return ["Heading", "Commodity Code", "Description", "Quantity", "MFN"]
+        elif num_columns == 6:
+            return ["Heading", "Commodity Code", "Description", "Quantity", "General", "MFN"]
+        elif num_columns == 7:
+            return ["Heading", "Commodity Code", "Description", "Unamed", "Quantity", "General", "MFN"]
+        # elif num_columns == 8:
+        #     return ["Heading", "Commodity Code", "Description", "Quantity", "General", "MFN", "Other", "Other"]
+
+        else:
+            raise ValueError(f"Invalid number of columns: {num_columns}")
+
+    def _process_type_1_row(self, row: pd.Series):
+        """
+        Process Type 1 type
+        """
+        print("****** start : tarriff row type 1******")
+
+        row = row.dropna()
+
+        print("****** end : tarriff row type 1******")
+
+        tarriff = Tarriff(
+            heading=row.get("Heading"),
+            commodity_code=row.get("Commodity Code", ""),
+            description=row.get("Description", ""),
+            statistical_unit=row.get("Quantity", ""),
+            general_rate_of_duty=row.get("General", ""),
+            mtf_rate_of_duty=row.get("MFN", ""),
+        )
+        tarriff.save()
+
+    def _process_type_2_row(self, row: pd.Series):
+        """
+        Process Type 1 type
+        """
+        print("****** start : tarriff row type 2 ******")
+        print(row)
+
+        row = row.dropna()
+        # print(row)
+
+        general = ""
+        unit = ""
+        if row.get("Quantity"):
+            digits = [(i, c) for i, c in enumerate(row.get("Quantity")) if c.isdigit()]
+            print("-- digits :" + str(digits))
+            print("qty : " + row.get("Quantity"))
+
+            if len(digits) > 1:
+                general = row.get("Quantity")[digits[1][0] :]
+                unit = row.get("Quantity")[: 1 * digits[1][0]]
+
+        tarriff = Tarriff(
+            heading=row.get("Heading"),
+            commodity_code=row.get("Commodity Code", ""),
+            description=row.get("Description", ""),
+            statistical_unit=unit or row.get("Quantity", ""),
+            general_rate_of_duty=row.get("General", "") or general,
+            mtf_rate_of_duty=row.get("MFN", ""),
+        )
+        tarriff.save()
+
+    def _process_type_3_row(self, row: pd.Series):
+        """
+        Process Type 3 type
+        """
+        print("****** start : tarriff row type 2 ******")
+        print(row)
+
+        row = row.dropna()
+        print(row)
+
+        tarriff = Tarriff(
+            heading=row.get("Heading"),
+            commodity_code=row.get("Commodity Code", ""),
+            description=row.get("Description", ""),
+            statistical_unit=row.get("Quantity"),
+            general_rate_of_duty=row.get("General"),
+            mtf_rate_of_duty=row.get("MFN", ""),
+        )
+        tarriff.save()
+
+    def _process_type_4_row(self, row: pd.Series):
+        """
+        Process Type 4 type
+        """
+        print("****** start : tarriff row type 2 ******")
+        print(row)
+
+        row = row.dropna()
+        print(row)
+
+        general = ""
+        unit = ""
+        if row[2]:
+            digits = [(i, c) for i, c in enumerate(row[2]) if c.isdigit()]
+            print("-- digits :" + str(digits))
+            print("qty : " + row.get("Quantity"))
+
+            if len(digits) > 1:
+                general = row.get("Quantity")[digits[1][0] :]
+                unit = row.get("Quantity")[: 1 * digits[1][0]]
+
+        tarriff = Tarriff(
+            heading=row[0],
+            commodity_code=row[0],
+            description=row[1],
+            statistical_unit=unit or row[2],
+            general_rate_of_duty=row[3] or general,
+            mtf_rate_of_duty=row[4],
+        )
+        tarriff.save()
+
+    def _process_section_header_row(self, row: pd.Series):
+        """
+        Process Type 3 type
+        """
+
+        if row.get("Heading") and row.Heading != "Heading" and str(row.Heading) != "nan":
+            print("****** section row ******")
+            print(row)
+            chapter_section = ChapterSection(
+                chapter=str(row.get("Heading")).split(".")[0],
+                section=row.get("Heading"),
+                description=row.get("Description"),
+            )
+            chapter_section.save()
+
+    def _write_axes(self, df):
+        """
+        Write Axes
+        """
+        # write axes
+        # ['0104.10.10', '- - -Pure-bred breeding sheep', '1. Kg', '0%', '0%.1']
+        if len(df.columns) == 4:
+            print(df.axes[1].tolist())
+            tarriff = Tarriff(
+                heading=df.axes[1].tolist()[0],
+                commodity_code=df.axes[1].tolist()[1],
+                description=df.axes[1].tolist()[2],
+                statistical_unit=df.axes[1].tolist()[3],
+                general_rate_of_duty=df.axes[1].tolist()[3],
+                # mtf_rate_of_duty=data[5],
+            )
+            tarriff.save()
+
+    def _get_page_type(self, number_of_columns: int):
+
+        if number_of_columns == 5:
+            return "type_1"
+        elif number_of_columns == 6:
+            return "type_2"
+        elif number_of_columns == 7:
+            return "type_3"
+
+        else:
+            raise ValueError(f"Unknown type with: {number_of_columns} columns")
+
+    def process_page_data(self, df):
+        """
+        Process the page
+        """
+        page_type = self._get_page_type(number_of_columns=len(df.columns))
+
+        try:
+            df.columns = self.map_columns(len(df.columns))
+        except ValueError:
+            print("skipping page : it has " + str(len(df.columns)) + "columns")
+
+        for row in df.iterrows():
+            data = row[1]
+
+            if "No." in data.index.values or "Heading" in data.index.values:
+
+                print("****** data row ******")
+                # print(data)
+                print("type : " + str(type(row[1])))
+                print("**********************")
+
+                if page_type == "type_1":
+                    self._process_type_1_row(data)
+                elif page_type == "type_2":
+                    self._process_type_2_row(data)
+                elif page_type == "type_3":
+                    self._process_type_3_row(data)
+
     def generate_tarriffs(self):
-        # Read pdf into DataFrame
-        list_of_df = tabula.read_pdf("zimra.pdf", pages="18-20")
+        try:
+            list_of_df = tabula.read_pdf(self.PDF_FILE_NAME, pages=self.PDF_PAGE_RANGE)
+            tabula.convert_into(
+                self.PDF_FILE_NAME, "test.csv", output_format="csv", stream=True, pages=self.PDF_PAGE_RANGE
+            )
 
-        columns = []
-
-        # print(list_of_df)
-
-        for idx, df in enumerate(list_of_df):
-
-            if isinstance(df, pd.DataFrame):
-                print(f"Page number : {idx} has {len(df.columns)} columns")
-                # print(df)
-
-                columns.append(len(df.columns))
-
-                if len(df.columns) == 5:
-                    # Type one page
-
-                    df.columns = ["Heading", "Commodity Code", "Description", "Quantity", "General"]
-                elif len(df.columns) == 6:
-                    # Type one page
-                    df.columns = ["Heading", "Commodity Code", "Description", "Quantity", "General", "Other Column"]
-
-                for row in df.iterrows():
-                    data = row[1]
-                    # print('********************************************************************************************************')
-                    # print(data)
-                    # print('********************************************************************************************************')
-
-                    if data.Heading == "Heading":
-                        # print("****** header row ******")
-                        # print(data)
-                        # print('********************************************************************************************************')
-                        continue
-                    if data.value_counts().size == 2:
-                        # print("****** sub-heading row ******")
-                        # print(data)
-                        print("****** sub-heading row ******")
-                        if data.Heading and data.Heading != 'Heading' and str(data.Heading) != 'nan':
-                            print(data)
-                            chapter_section = ChapterSection(
-                                chapter=str(data["Heading"]).split(".")[0],
-                                section=data["Heading"],
-                                description=data["Description"],
-                            )
-                            chapter_section.save()
-                            print('********************************************************************************************************')
-                        # print('********************************************************************************************************')
-                        continue
-                    else:
-                        print("****** tarriff row ******")
-                        print(data)
-
-                        tarriff = Tarriff(
-                            heading=data["Heading"],
-                            commodity_code=data["Commodity Code"],
-                            description=data["Description"],
-                            statistical_unit=data["Quantity"],
-                            general_rate_of_duty=data["General"],
-                            mtf_rate_of_duty=data["General"],
-                        )
-                        tarriff.save()
-                        print(
-                            "********************************************************************************************************"
-                        )
-                        continue
-            else:
-                print(f"Skipping Page number : {idx}")
-                continue
-
-        print("unique columns : %s", set(columns))
-
-        # print(list_of_df)
-        count = 0
+            # print(list_of_df)
+        except Exception as e:
+            print(f"An error occurred while reading the pdf file: {e}")
+            return
 
         for idx, df in enumerate(list_of_df):
-            if count > 10:
-                break
-
             if isinstance(df, pd.DataFrame):
                 print(f"Page number : {idx} has {len(df.columns)} columns")
+                print(df.axes[1].tolist())
+                print(tabulate(df, headers="keys", tablefmt="psql"))
                 # print(df)
 
-                columns.append(len(df.columns))
-
-                if len(df.columns) == 5:
-                    # Type one page
-
-                    df.columns = ["Heading", "Commodity Code", "Description", "Quantity", "General"]
-                elif len(df.columns) == 6:
-                    # Type one page
-                    df.columns = ["Heading", "Commodity Code", "Description", "Quantity", "General", "Other Column"]
-
-                elif len(df.columns) == 10:
-                    # Type 2
-                    df.columns = [
-                        "heading",
-                        "CD",
-                        "article_description",
-                        "statistical_unit",
-                        "general_rate_of_duty",
-                        "eu_rate_of_duty",
-                        "efta_rate_of_duty",
-                        "sadc_rate_of_duty",
-                        "unknown_1",
-                        "mercosur_rate_of_duty",
-                    ]
-
-                elif len(df.columns) == 12:
-                    # Type 3
-                    df.columns = [
-                        "heading",
-                        "CD",
-                        "article_description",
-                        "statistical_unit",
-                        "general_rate_of_duty",
-                        "eu_rate_of_duty",
-                        "efta_rate_of_duty",
-                        "sadc_rate_of_duty",
-                        "unknown_1",
-                        "mercosur_rate_of_duty",
-                        "unknown_2",
-                        "unknown_3",
-                    ]
-
-                for row in df.iterrows():
-                    data = row[1]
-                    # print('********************************************************************************************************')
-                    # print(data)
-                    # print('********************************************************************************************************')
-
-                    if data.Heading == "Heading":
-                        # print("****** header row ******")
-                        # print(data)
-                        # print('********************************************************************************************************')
-                        continue
-                    if data.value_counts().size == 2:
-                        # print("****** sub-heading row ******")
-                        # print(data)
-                        # print('********************************************************************************************************')
-                        continue
-                    else:
-                        print("****** tarriff row ******")
-                        print(data)
-                        print(
-                            "********************************************************************************************************"
-                        )
-                        continue
-            else:
-                print(f"Skipping Page number : {idx}")
-                continue
-
-        print("unique columns : %s", set(columns))
+                print("---------------------------------")
+                self.process_page_data(df)
 
     def handle(self, *args, **options):
         self.stdout.write("Seeding tarriffs...")
         self.generate_tarriffs()
-        self.stdout.write("Tarriffs saved !!!")
+        self.stdout.write("\n\nTarriffs saved !!!")
--- taxwise/settings.py
+++ taxwise/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = "taxwise.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "taxwise",
+        "USER": "root",
+        "PASSWORD": "root",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
+        "OPTIONS": {},
     }
 }
 
